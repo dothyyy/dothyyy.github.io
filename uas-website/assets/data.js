067ee01ba2bf8c547e8e78db4b0c0fa22936155b
dothyyy <124017545+dothyyy@users.noreply.github.com>
2023-05-02 01:27:11 +0700

    Add files via upload

---
+++ uas-website/assets/data.js
@@ -0,0 +1,449 @@
+var items = [
+    {
+        name : 'Matilda Top Voyager',
+        price : "Rp 239.000",
+        link : 'assets/img/matilda top voyager.png',
+        gender : 'girls',
+        type : "top",
+        id : 1,
+    },
+
+    {
+        name : 'Landon Vest Taupe',
+        price : "Rp 108.000",
+        link : 'assets/img/landon vest taupe.png',
+        gender : 'girls',
+        type : "top",
+        id : 2
+    },
+
+    {
+        name : 'Lily Square Neck Top Red',
+        price : "Rp 78.000",
+        link : 'assets/img/lily square neck red.png',
+        gender : 'girls',
+        type : "top",
+        id : 3
+    },
+
+    {
+        name : 'V-Ribbon Top Blue',
+        price : "Rp 68.000",
+        link : 'assets/img/v ribbon top blue.png',
+        gender : 'girls',
+        type : "top",
+        id : 4
+    },
+
+    {
+        name : 'V-Ribbon Top Lilac',
+        price : "Rp 68.000",
+        link : 'assets/img/v ribbon top lilac.png',
+        gender : 'girls',
+        type : "top",
+        id : 5
+    },
+
+    {
+        name : 'Seina Peach Romper',
+        price : "Rp 250.000",
+        link : 'assets/img/seina peach romper.png',
+        gender : 'girls',
+        type : "set",
+        id : 6
+    },
+
+    {
+        name : 'Landon Vest Artic',
+        price : "Rp 108.000",
+        link : 'assets/img/landon vest artic.png',
+        gender : 'girls',
+        type : "top",
+        id : 7
+    },
+
+    {
+        name : 'Riku Set Peanut',
+        price : "Rp 148.000",
+        link : 'assets/img/riku set peanut.png',
+        gender : 'boys',
+        type : "set",
+        id : 8
+    },
+
+    {
+        name : 'Mason Top Sage',
+        price : "Rp 128.000",
+        link : 'assets/img/mason top sage.png',
+        gender : 'boys',
+        type : "top",
+        id : 9
+    },
+
+    {
+        name : 'Eli Top Green',
+        price : "Rp 379.000",
+        link : 'assets/img/eli top green.png',
+        gender : 'boys',
+        type : "top",
+        id : 10
+    },
+
+    {
+        name : 'Bosa Corduroy Pants Blue',
+        price : "Rp 279.000",
+        link : 'assets/img/bosa corduroy pants blue.png',
+        gender : 'boys',
+        type : "bottom",
+        id : 11
+    },
+
+    {
+        name : 'Terry Jogger Pants Almond',
+        price : "Rp 99.000",
+        link : 'assets/img/terry jogger pants almond.png',
+        gender : 'boys',
+        type : "bottom",
+        id : 12
+    },
+
+    {
+        name : 'Ebony Pants Black',
+        price : "Rp 179.000",
+        link : 'assets/img/ebony pants black.png',
+        gender : 'boys',
+        type : "bottom",
+        id : 13
+    },
+
+    {
+        name : 'Moe Set Khaki',
+        price : "Rp 255.000",
+        link : 'assets/img/moe set khaki.png',
+        gender : 'boys',
+        type : "set",
+        id : 14
+    },
+
+    {
+        name : 'Dottie Set',
+        price : "Rp 200.000",
+        link : 'assets/img/dottie set.png',
+        gender : 'girls',
+        type : "set",
+        id : 15
+    },
+
+    {
+        name : 'Riku Set Peanut',
+        price : "Rp 108.000",
+        link : 'assets/img/riku set peanut.png',
+        gender : 'girls',
+        type : "set",
+        id : 16
+    },
+
+    {
+        name : 'Kumi Shirt Navy',
+        price : "Rp 95.000",
+        link : 'assets/img/kumi shirt navy.png',
+        gender : 'girls',
+        type : "top",
+        id : 17
+    },
+    {
+        name : 'Augie Skirt Peach',
+        price : "Rp 199.000",
+        link : 'assets/img/augie skirt peach.png',
+        gender : 'girls',
+        type : "bottom",
+        id : 18
+    },
+
+    {
+        name : 'Iris Set Lilac',
+        price : "Rp 305.000",
+        link : 'assets/img/Iris_Set_Lilac.png',
+        gender : 'girls',
+        type : "set",
+        id : 19
+    },
+
+    {
+        name : 'Elidom Pink',
+        price : "Rp 99.000",
+        link : 'assets/img/Elidom_Tops_in_Pink_.png',
+        gender : 'girls',
+        type : "top",
+        id : 20
+    },
+
+    {
+        name : 'Sersya Mustard',
+        price : "Rp 107.000",
+        link : 'assets/img/Sersya_Tops_Mustard.png',
+        gender : 'girls',
+        type : "top",
+        id : 21
+    },
+
+    {
+        name : 'Ifa Set Peach',
+        price : "Rp 109.000",
+        link : 'assets/img/Ifa_Set_in_Peach_.png',
+        gender : 'girls',
+        type : "set",
+        id : 22
+    },
+
+    {
+        name : 'Rio Denim',
+        price : "Rp 375.000",
+        link : 'assets/img/rio denim.png',
+        gender : 'boys',
+        type : "set",
+        id : 23
+    },
+
+    {
+        name : 'Solo White',
+        price : "Rp 179.000",
+        link : 'assets/img/solo white.png',
+        gender : 'boys',
+        type : "top",
+        id : 24
+    },
+
+    {
+        name : 'Rae Knit Skirt Lilac',
+        price : "Rp 189.000",
+        link : 'assets/img/rae knit skirt.png',
+        gender : 'girls',
+        type : "bottom",
+        id : 25
+    },
+
+    {
+        name : 'Lea Corduroy Purple',
+        price : "Rp 165.000",
+        link : 'assets/img/lea corduroy purple.png',
+        gender : 'girls',
+        type : "bottom",
+        id : 26
+    },
+
+    {
+        name : 'Zoe Short Yellow',
+        price : "Rp 105.000",
+        link : 'assets/img/zoe short yellow.png',
+        gender : 'girls',
+        type : "bottom",
+        id : 27
+    },
+
+    {
+        name : 'Poppy Biker Short Green',
+        price : "Rp 89.000",
+        link : 'assets/img/poppy biker short.png',
+        gender : 'girls',
+        type : "bottom",
+        id : 28
+    },
+
+    {
+        name : 'Terra Short Latte',
+        price : "Rp120.000",
+        link : 'assets/img/terra short latte.png',
+        gender : 'girls',
+        type : "bottom",
+        id : 29
+    },
+
+    {
+        name : 'Mia Skort Nude',
+        price : "Rp 199.000",
+        link : 'assets/img/mia skort nude.png',
+        gender : 'girls',
+        type : "bottom",
+        id : 30
+    },
+    {
+        name : 'Lora Skort BW',
+        price : "Rp 129.000",
+        link : 'assets/img/lora bw.png',
+        gender : 'girls',
+        type : "bottom",
+        id : 31
+    },
+
+    {
+        name : 'Kyle Shorts',
+        price : "Rp 109.000",
+        link : 'assets/img/KYle short.jpeg',
+        gender : 'boys',
+        type : "bottom",
+        id : 32
+    },
+
+    {
+        name : 'Aslan Seashell',
+        price : "Rp 118.000",
+        link : 'assets/img/Aslan_Top_Seashell.png',
+        gender : 'boys',
+        type : "top",
+        id : 33
+    },
+
+    {
+        name : 'Eugene Set Black',
+        price : "Rp 369.000",
+        link : 'assets/img/Eugene_Set_in_Black.png',
+        gender : 'boys',
+        type : "set",
+        id : 34
+    },
+
+    {
+        name : 'Aurora Set Pink ',
+        price : "Rp 185.000",
+        link : 'assets/img/AURORA_SET_PINK.png',
+        gender : 'girls',
+        type : "set",
+        id : 35
+    },
+
+    {
+        name : 'Poro Navy',
+        price : "Rp 135.000",
+        link : 'assets/img/Poro_Oversized_T-Shirt_in_Navy_.png',
+        gender : 'girls',
+        type : "top",
+        id : 36
+    },
+
+    {
+        name : 'Mindi BW',
+        price : "Rp 107.000",
+        link : 'assets/img/Mindi_Tops_in_Broken_White.png',
+        gender : 'girls',
+        type : "top",
+        id : 37
+    },
+
+    {
+        name : 'Lulu White',
+        price : "Rp 109.000",
+        link : 'assets/img/LULU_TOP_WHITE_.png',
+        gender : 'girls',
+        type : "top",
+        id : 38
+    },
+
+    {
+        name : 'Luca Set Blue',
+        price : "Rp 429.000",
+        link : 'assets/img/Luca_Set_Blue.png',
+        gender : 'boys',
+        type : "set",
+        id : 39
+    },
+
+    {
+        name : 'Clove Olive',
+        price : "Rp 119.000",
+        link : 'assets/img/CLOVE_SHORT_GREEN.png',
+        gender : 'boys',
+        type : "bottom",
+        id : 40
+    },
+
+    {
+        name : 'Two Color Lilac',
+        price : "Rp 78.000",
+        link : 'assets/img/Two_Color_Short_Lilac1.png',
+        gender : 'boys',
+        type : "bottom",
+        id : 41
+    },
+
+    {
+        name : 'Kyo Short Camel',
+        price : "Rp 139.000",
+        link : 'assets/img/Kyo_Shorts.png',
+        gender : 'boys',
+        type : "bottom",
+        id : 42
+    }
+]
+
+var avail = [
+    {
+        name : 'Eli Top Green',
+        price : "Rp 379.000",
+        link : 'assets/img/eli top green.png',
+        gender : 'boys',
+        type : "top",
+        id : 10,
+        avail : 'false'
+    },
+
+    {
+        name : 'Bosa Corduroy Pants Blue',
+        price : "Rp 279.000",
+        link : 'assets/img/bosa corduroy pants blue.png',
+        gender : 'boys',
+        type : "bottom",
+        id : 11,
+        avail : 'true'
+    },
+
+    {
+        name : 'Terry Jogger Pants Almond',
+        price : "Rp 99.000",
+        link : 'assets/img/terry jogger pants almond.png',
+        gender : 'boys',
+        type : "bottom",
+        id : 12,
+        avail : 'false'
+    },
+
+    {
+        name : 'Ebony Pants Black',
+        price : "Rp 179.000",
+        link : 'assets/img/ebony pants black.png',
+        gender : 'boys',
+        type : "bottom",
+        id : 13,
+        avail : 'true'
+    },
+
+    {
+        name : 'Moe Set Khaki',
+        price : "Rp 255.000",
+        link : 'assets/img/moe set khaki.png',
+        gender : 'boys',
+        type : "set",
+        id : 14,
+        avail : 'false'
+    },
+
+    {
+        name : 'Kumi Shirt Navy',
+        price : "Rp 95.000",
+        link : 'assets/img/kumi shirt navy.png',
+        gender : 'girls',
+        type : "top",
+        id : 17,
+        avail : 'false'
+    },
+
+    {
+        name : 'Augie Skirt Peach',
+        price : "Rp 199.000",
+        link : 'assets/img/augie skirt peach.png',
+        gender : 'girls',
+        type : "bottom",
+        id : 18,
+        avail : 'true'
+    }
+]
